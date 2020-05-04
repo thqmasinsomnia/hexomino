309d71ba856304aa46d23544ffc5e134874f4cce
thqmasinsomnia <tsmith142@saddleback.edu>
2020-05-03 22:55:54 -0700

    more stuf

--- hexomino.js
+++ hexomino.js
@@ -36,12 +36,31 @@ function draw() {
             peiceNum = 0;
         }
     } else {
+
+      
+
         for (var y = 0; y < 6; y++) {
             for (var x = 0; x < 6; x++) {
-     
+
+
+
+
                 if (peices[peiceNum][x][y] != 0) {
-                    fill(255, 0, 0);
-                    rect(y * 100, x * 100, 100, 100);
+
+                    if (peiceColor[peiceNum][1] == 'G') {
+                        fill(0, 255, 0);
+                        rect(y * 100, x * 100, 100, 100);
+                    } else if (peiceColor[peiceNum][1] == 'R') {
+                        fill(255, 0, 0);
+                        rect(y * 100, x * 100, 100, 100);
+                    } else if (peiceColor[peiceNum][1] == 'B') {
+                        fill(0, 0, 255);
+                        rect(y * 100, x * 100, 100, 100);
+                    } else {
+                        fill(255, 255, 0);
+                        rect(y * 100, x * 100, 100, 100);
+                    }
+                   
                 }
             }
         }
@@ -54,16 +73,68 @@ function draw() {
     background(0);
     for (var y = 0; y < 15; y++) {
         for (var x = 0; x < 15; x++) {
-            if (board[x][y] != 0) {
+            if (board[x][y] == 'X') {
                 fill(255);
+                rect(y * 100, x * 100, 100, 100);
+            }
+            else if (board[x][y] != 0) {
+                fill(255,0,0);
                 rect(y * 100, x * 100, 100, 100);
             }
         }
     }
 }
+
+
+hex();
 */
 
+//This function is to add a selected peice to the board
+function addPeice(peiceNum) {
+    for (var y = 0; y < 15; y++) {
+        for (var x = 0; x < 15; x++) {
+            if (peices[peiceNum][y][x] != 0) {
+
+            }
+                
+        }
+    }
+
+}
+
+
+/*
+function hex() {
+    const peiceLoc = [];
+    var peiceSel = 0;
+    var done = false;
+      while (!done) {
+
+       
+
+
+          for (var y = 0; y < 15; y++) {
+              for (var x = 0; x < 15; x++) {
+
+              }
+          }
 
 
 
 
+
+        done = true;
+        for (var y = 0; y < 15; y++) {
+            for (var x = 0; x < 15; x++) {
+                if (board[x][y] == 0) {
+                    done = false;
+                }
+            }
+        }
+
+
+    }
+
+   
+}
+*/
